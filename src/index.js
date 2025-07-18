7f8cfa40fa2c611d246b801c68e8f1c76d9cf806
Rithvik Pallamreddy <1017rithvik@gmail.com>
2025-07-18 18:05:16 -0500

    db setup and user schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const { connectDB } = require("./config/database");
+
+app.use("/api/users", (req, res) => {
+  res.send("Hello World from the users route");
+});
+
+app.use("/api/users/:id", (req, res) => {
+  res.send(`Hello World from the users route with id ${req.params.id}`);
+});
 
 app.use("/api", (req, res) => {
   res.send("Hello World from the server");
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+// Database connection and listen to the port 3001.
+connectDB().then(() => {
+  console.log("MongoDB connected");
+  app.listen(3001, () => {
+    console.log("Server is successfully listening on port 3001");
+  });
 });
---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
